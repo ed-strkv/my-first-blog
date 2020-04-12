dd56ff6024eaa23e09e6f229fbd99e8b34c4bcf3
Eduard Striukov <ed.strkv@gmail.com>
2020-04-12 14:45:42 +0300

    Added LiveReload, Emmet and other packages.

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    for post in posts:
+        approved_comment_counter = post.comments.filter(approved_comment=True).count()
+        all_comment_counter = post.comments.all().count()
+        #approved_percent =(approved_comment_counter*100)/all_comment_counter
+        print(all_comment_counter, approved_comment_counter)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
